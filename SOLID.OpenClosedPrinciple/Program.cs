66e2ebf504c19af2a2e11615c8968a7eb246e40c
raihanM95 <mahmud.raihan@yahoo.com>
2021-11-04 11:39:10 +0600

    Open Closed Principle

---
+++ SOLID.OpenClosedPrinciple/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+//public class Account
+//{
+//    public decimal Interest { get; set; }
+//    public decimal Balance { get; set; }
+//    // members and function declaration
+//    public decimal CalcInterest(string accType)
+//    {
+
+//        if (accType == "Regular") // savings
+//        {
+//            Interest = (Balance * 4) / 100;
+//            if (Balance < 1000) Interest -= (Balance * 2) / 100;
+//            if (Balance < 50000) Interest += (Balance * 4) / 100;
+//        }
+//        else if (accType == "Salary") // salary savings
+//        {
+//            Interest = (Balance * 5) / 100;
+//        }
+//        else if (accType == "Corporate") // Corporate
+//        {
+//            Interest = (Balance * 3) / 100;
+//        }
+//        return Interest;
+//    }
+//}
+
+/* We can apply OCP by using interface, abstract class, abstract methods and virtual methods when you want to extend functionality. */
+
+interface IAccount
+{
+    // members and function declaration, properties
+    decimal Balance { get; set; }
+    decimal CalcInterest();
+}
+
+//regular savings account 
+public class RegularSavingAccount : IAccount
+{
+    public decimal Balance { get; set; } = 0;
+    public decimal CalcInterest()
+    {
+        decimal Interest = (Balance * 4) / 100;
+        if (Balance < 1000) Interest -= (Balance * 2) / 100;
+        if (Balance < 50000) Interest += (Balance * 4) / 100;
+
+        return Interest;
+    }
+}
+
+//Salary savings account 
+public class SalarySavingAccount : IAccount
+{
+    public decimal Balance { get; set; } = 0;
+    public decimal CalcInterest()
+    {
+        decimal Interest = (Balance * 5) / 100;
+        return Interest;
+    }
+}
+
+//Corporate Account
+public class CorporateAccount : IAccount
+{
+    public decimal Balance { get; set; } = 0;
+    public decimal CalcInterest()
+    {
+        decimal Interest = (Balance * 3) / 100;
+        return Interest;
+    }
+}
--- SOLID.SingleResponsibilityPrinciple/Program.cs
+++ SOLID.SingleResponsibilityPrinciple/Program.cs
@@ -1,2 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//public class GarageStation
+//{
+//    public void DoOpenGate()
+//    {
+//        //Open the gate functinality
+//    }
+
+//    public void PerformService(Vehicle vehicle)
+//    {
+//        //Check if garage is opened
+//        //finish the vehicle service
+//    }
+
+//    public void DoCloseGate()
+//    {
+//        //Close the gate functinality
+//    }
+//}
+
+/* We can correctly apply SRP by refactoring of above code by introducing interface */
+
+public class GarageStation
+{
+    IGarageUtility _garageUtil;
+
+    public GarageStation(IGarageUtility garageUtil)
+    {
+        this._garageUtil = garageUtil;
+    }
+    public void OpenForService()
+    {
+        _garageUtil.OpenGate();
+    }
+    public void DoService()
+    {
+        //Check if service station is opened and then
+        //finish the vehicle service
+    }
+    public void CloseGarage()
+    {
+        _garageUtil.CloseGate();
+    }
+}
+public class GarageStationUtility : IGarageUtility
+{
+    public void OpenGate()
+    {
+        //Open the Garage for service
+    }
+
+    public void CloseGate()
+    {
+        //Close the Garage functionlity
+    }
+}
+
+public interface IGarageUtility
+{
+    void OpenGate();
+    void CloseGate();
+}
