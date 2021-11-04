5567b9d6d8c79e1cfc3c1ff865aa5d51731ce837
raihanM95 <mahmud.raihan@yahoo.com>
2021-11-04 11:46:48 +0600

    Interface Segregation Principle

---
+++ SOLID.InterfaceSegregationPrinciple/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+//public interface IOrder
+//{
+//    void AddToCart();
+//    void CCProcess();
+//}
+
+//public class OnlineOrder : IOrder
+//{
+//    public void AddToCart()
+//    {
+//        //Do Add to Cart
+//    }
+
+//    public void CCProcess()
+//    {
+//        //process through credit card
+//    }
+//}
+
+//public class OfflineOrder : IOrder
+//{
+//    public void AddToCart()
+//    {
+//        //Do Add to Cart
+//    }
+
+//    public void CCProcess()
+//    {
+//        //Not required for Cash/ offline Order
+//        throw new NotImplementedException();
+//    }
+//}
+
+/* We can resolve this violation by dividing IOrder Interface. */
+
+public interface IOrder
+{
+    void AddToCart();
+}
+
+public interface IOnlineOrder
+{
+    void CCProcess();
+}
+
+public class OnlineOrder : IOrder, IOnlineOrder
+{
+    public void AddToCart()
+    {
+        //Do Add to Cart
+    }
+
+    public void CCProcess()
+    {
+        //process through credit card
+    }
+}
+
+public class OfflineOrder : IOrder
+{
+    public void AddToCart()
+    {
+        //Do Add to Cart
+    }
+}
